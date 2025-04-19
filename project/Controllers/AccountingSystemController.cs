99a3f994e463952583c36ab46ebcef28ef6d88c2
Zixun55 <b0979433390@gmail.com>
2025-04-20 00:56:39 +0800

    帳本資料頁面

--- project/Controllers/AccountingSystemController.cs
+++ project/Controllers/AccountingSystemController.cs
@@ -13,13 +13,36 @@ namespace project.Controllers
         {
             _configuration = configuration;
         }
+
+        /// <summary>
+        /// 獲得帳本列表
+        /// </summary>
+        /// <returns></returns>
         public ActionResult AccountBookList()
         {
             AccountBookService service = new AccountBookService(_configuration);
             var searchArg = new AccountBookList { UserId = 1 };
-            List<AccountBookList> listResult = service.SearchBookData(searchArg);
+            List<AccountBookList> listResult = service.GetAccountBookList(searchArg);
 
             return View(listResult);
+        }
+
+        /// <summary>
+        /// 獲得帳本資料
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AccountBookData(int id)
+        {
+            AccountBookService service = new AccountBookService(_configuration);
+            var searchArg = new TransactionList { AccountBookId = id };
+            List<TransactionList> accountBookDataResult = service.GetAccountBookData(searchArg);
+
+            if (accountBookDataResult == null || accountBookDataResult.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return View(accountBookDataResult);
         }
     }
 }
--- project/Models/Services/AccountBookService.cs
+++ project/Models/Services/AccountBookService.cs
@@ -55,5 +55,41 @@ namespace project.Models.Services
 
             return ListResult;
         }
+
+        /// <summary>
+        /// 帳本資料
+        /// </summary>
+        /// <param name="arg">帳本編號</param>
+        /// <returns></returns>
+        public List<Models.TransactionList> GetAccountBookData(Models.TransactionList arg)
+        {
+            DataTable dt = new DataTable();
+            string sql = @"SELECT TRANSACTION_ID, ACCOUNT_BOOK_ID, DATE, AMOUNT, DESCRIPTION, TRANSACTION_CURRENCY, CATEGORY FROM TRANSACTION WHERE ACCOUNT_BOOK_ID = @ACCOUNT_BOOK_ID";
+            using (NpgsqlConnection conn = new NpgsqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ACCOUNT_BOOK_ID", arg.AccountBookId);
+                NpgsqlDataAdapter sqlAdapter = new NpgsqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+            List<Models.TransactionList> ListResult = new List<TransactionList>();
+            foreach (DataRow row in dt.Rows)
+            {
+                ListResult.Add(new TransactionList()
+                {
+                    TransactionId = Convert.ToInt32(row["TRANSACTION_ID"]),
+                    AccountBookId = Convert.ToInt32(row["ACCOUNT_BOOK_ID"]),
+                    Date = Convert.ToDateTime(row["DATE"]),
+                    Description = Convert.ToString(row["Description"]),
+                    Category = Convert.ToString(row["CATEGORY"]),
+                    Amount = Convert.ToInt32(row["AMOUNT"]),
+                    Currency = Convert.ToString(row["TRANSACTION_CURRENCY"])
+                });
+            }
+
+            return ListResult;
+        }
     }
 }
