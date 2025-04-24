234597ccc8944d9f2734d4134d6c5c985627db0d
Zixun55 <b0979433390@gmail.com>
2025-04-24 14:43:40 +0800

    修正"新增帳本"功能的必填提示訊息

--- project/Models/AccountBookData.cs
+++ project/Models/AccountBookData.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace project.Models
 {
@@ -8,13 +9,16 @@ namespace project.Models
         public int UserId { get; set; }
 
         [DisplayName("帳本名稱")]
+        [Required(ErrorMessage = "帳本名稱未填寫")]
         public string AccountBookName { get; set; }
 
         [DisplayName("帳本描述")]
+        [Required(ErrorMessage = "帳本描述未填寫")]
         public string Description { get; set; }
 
 
         [DisplayName("貨幣單位")]
+        [Required(ErrorMessage = "貨幣單位未填寫")]
         public string BaseCurrency { get; set; }
     }
 }
