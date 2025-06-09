5a2750b34b79acffe5f357366a44c85b6e5624c4
Zixun55 <b0979433390@gmail.com>
2025-06-09 15:17:48 +0800

    transaction及user的class改為繼承

--- project/Models/TransactionData.cs
+++ project/Models/TransactionData.cs
@@ -4,13 +4,44 @@ using System.ComponentModel.DataAnnotations;
 namespace project.Models
 {
     /// <summary>
-    /// 交易資料
+    /// Transaction基本欄位
     /// </summary>
-    public class TransactionData
+    public class BaseTransactionData
     {
         [DisplayName("交易編號")]
         public int TransactionId { get; set; }
 
+        [DisplayName("交易日期")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("交易類別")]
+        public string Category { get; set; }
+
+        [DisplayName("交易描述")]
+        public string Description { get; set; }
+
+        [DisplayName("交易金額")]
+        public int Amount { get; set; }
+
+        [DisplayName("交易幣別")]
+        public string Currency { get; set; }
+
+        [DisplayName("帳本編號")]
+        public int AccountBookId { get; set; }
+    }
+
+    /// <summary>
+    //  交易列表(帳本資料)
+    //  </summary>
+    public class TransactionList: BaseTransactionData
+    {
+    }
+
+    /// <summary>
+    /// 交易資料
+    /// </summary>
+    public class TransactionData : BaseTransactionData
+    {
         [Required(ErrorMessage = "日期未填寫")]
         [DisplayName("交易日期")]
         public DateTime Date { get; set; }
@@ -30,9 +61,6 @@ namespace project.Models
         [Required(ErrorMessage = "幣別未填寫")]
         [DisplayName("交易幣別")]
         public string Currency { get; set; }
-
-        [DisplayName("帳本編號")]
-        public int AccountBookId { get; set; }
 
         [DisplayName("是否加入預算")]
         public bool IncludeInBudget { get; set; }
--- project/Models/UserData.cs
+++ project/Models/UserData.cs
@@ -4,14 +4,10 @@ using System.ComponentModel.DataAnnotations;
 namespace project.Models
 {
     /// <summary>
-    /// 註冊用使用者資料
+    /// User基本欄位：Email, Password
     /// </summary>
-    public class UserData
+    public class BaseUserData
     {
-        [Required(ErrorMessage = "使用者名稱未填寫")]
-        [DisplayName("使用者名稱")]
-        public string UserName { get; set; }
-
         [Required(ErrorMessage = "Email未填寫")]
         [DisplayName("Email")]
         public string Email { get; set; }
@@ -19,5 +15,22 @@ namespace project.Models
         [Required(ErrorMessage = "密碼未填寫")]
         [DisplayName("密碼")]
         public string Password { get; set; }
+    }
+
+    /// <summary>
+    /// 註冊用使用者資料
+    /// </summary>
+    public class UserData: BaseUserData
+    {
+        [Required(ErrorMessage = "使用者名稱未填寫")]
+        [DisplayName("使用者名稱")]
+        public string UserName { get; set; }
+    }
+
+    /// <summary>
+    /// 登入用使用者資料
+    /// </summary>
+    public class LoginData: BaseUserData
+    {
     }
 }
